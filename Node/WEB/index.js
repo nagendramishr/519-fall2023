56be2b6f8511c109721e38cb9a8e52564691dd40
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2023-10-12 15:18:04 -0400

    Update index.js

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -5,7 +5,7 @@ const port = process.env.WEBSITES_PORT || 8080;
 app.use('/', express.static('frontend/build'));
 
 app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world!  '+ provess.env.Secret1);
 });
 
 app.listen(port, () => {
