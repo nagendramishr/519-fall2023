7cceb0bf26b68d23669e4f92a87c4c5a532a19d5
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2023-10-13 10:51:29 -0400

    Update test1.cs

--- C_Sharp/API1/test1.cs
+++ C_Sharp/API1/test1.cs
@@ -18,12 +18,14 @@ namespace API1
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"Secret2 = {GetEnvironmentVariable("Secret2")}");
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
