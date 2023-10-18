bab2b2f77095afa877f6916761b3874370f6ad53
Nagendra Mishr <nagendra@microsoft.com>
2023-10-18 13:13:15 -0400

    added code to log value of secret

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    context.log('Secret2: '+ process.env['Secret2']);
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
